1a0a137adab08213cbf8aecae53d12a502a6ae44
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-08-01 03:27:25 +0530

    Admins and Apps

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Tutorial
 # Register your models here.
+
+admin.site.register(Tutorial)
--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
 app_name = "main"
 
 urlpatterns = [
-    url("",views.homepage, name="homepage"),
-   
+    url("/home",views.homepage, name="homepage"),
+    url(r'^admin/', admin.site.urls),
 ]
